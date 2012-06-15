7a1a5a2a55e3de46b5cf6f8b97dac404d6f2c8a9
Gavin Hogan <ghogan@commercehub.com>
2012-06-14 20:49:06 -0400

    Initial commit

---
+++ MetricsGrailsPlugin.groovy
@@ -0,0 +1,60 @@
+class MetricsGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.0 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/*",
+            "grails=app/domain/*",
+    ]
+
+    // TODO Fill in these fields
+    def title = "Metrics Plugin" // Headline display name of the plugin
+    def author = "Gavin Hogan"
+    def authorEmail = "gavinhogan@gmail.com"
+    def description = '''\
+Provides easy integration and usage of Coda Hale's/Yammer.com's Java metrics
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/grails-metrics"
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    def license = "APACHE"
+
+    // Details of company behind the plugin (if there is one)
+    //def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+
+    // Online location of the plugin's browseable source code.
+//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+
+    def doWithWebDescriptor = { xml ->
+    }
+
+    def doWithSpring = {
+    }
+
+    def doWithDynamicMethods = { ctx ->
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+    }
+
+    def onChange = { event ->
+    }
+
+    def onConfigChange = { event ->
+    }
+
+    def onShutdown = { event ->
+    }
+}
